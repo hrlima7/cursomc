ad8c5c016b3174ce7a69d2accb5fe6287966f0e3
Henrique Roma <hrlima7@gmail.com>
2020-04-12 23:25:28 -0300

    Inclusao class of domain

--- src/main/java/br/com/roma/resource/ResourceController.java
+++ src/main/java/br/com/roma/resource/ResourceController.java
@@ -1,15 +1,30 @@
 package br.com.roma.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.roma.domain.Categoria;
 
 @RestController
 public class ResourceController {
 
 	@RequestMapping("/categoria")
-	public String Categorizar() {
+	public List<Categoria> Categorizar() {
 		
-		return "REST Funcionando !";
+		Categoria cat1 = new Categoria(1,"celular");
+		Categoria cat2 = new Categoria(2, "NoteBook");
+		
+		
+		
+		List<Categoria> lista = new ArrayList();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 		
 	}
 }
